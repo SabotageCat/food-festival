457b47be7ba5b0a5dc6cee2ae92e29cbbead0588
Robert D Gonzales <robert.gonzo13@yahoo.com>
2022-06-09 17:32:11 -0500

    added offline functionality thru service-worker

--- service-worker.js
+++ service-worker.js
@@ -16,6 +16,15 @@ const FILES_TO_CACHE = [
     "./dist/schedule.bundle.js"
 ];
 
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+      caches.open(CACHE_NAME).then(function (cache) {
+        console.log('installing cache : ' + CACHE_NAME)
+        return cache.addAll(FILES_TO_CACHE)
+      })
+    )
+});
+
 self.addEventListener('activate', function(e) {
     e.waitUntil(
       caches.keys().then(function(keyList) {
@@ -29,9 +38,24 @@ self.addEventListener('activate', function(e) {
             if (cacheKeeplist.indexOf(key) === -1) {
               console.log('deleting cache : ' + keyList[i]);
               return caches.delete(keyList[i]);
-            }
-          })
-        );
+              }
+            })
+        )
       })
-    );
-  });
+    )
+});
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('responding with cache : ' + e.request.url);;
+                return request;
+            } else {
+                console.log('file is not cached, fetching : ' + e.request.url);
+                return fetch(e.request);
+            }
+        })
+    )
+});
